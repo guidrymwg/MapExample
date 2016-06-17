3593e8b5d06b3fae2dacb84433b084340c1f9d9d
guidrymwg <guidry@utk.edu>
2016-06-16 22:44:12 -0400

    adding xml and java files

--- app/src/main/java/com/lightcone/mapexample/MainActivity.java
+++ app/src/main/java/com/lightcone/mapexample/MainActivity.java
@@ -1,7 +1,14 @@
 package com.lightcone.mapexample;
 
-/**
- * Created by guidry on 6/16/16.
- */
-public class MainActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.activity_main);
+      }
+
 }
