75eb8ca7bb8ac12ede654b216f0ff7c93f928863
guidrymwg <guidry@utk.edu>
2016-06-25 16:07:15 -0400

    Changed Help and Settings displays to use AppCompatActivity rather than Activity for consistency.  Changed corresponding styles in manifest file to custom dialog style defined in styles.xml.

--- app/src/main/java/com/lightcone/mapexample/Help.java
+++ app/src/main/java/com/lightcone/mapexample/Help.java
@@ -2,8 +2,9 @@ package com.lightcone.mapexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class Help extends Activity {
+public class Help extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
--- app/src/main/java/com/lightcone/mapexample/Settings.java
+++ app/src/main/java/com/lightcone/mapexample/Settings.java
@@ -3,8 +3,9 @@ package com.lightcone.mapexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class Settings extends Activity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
